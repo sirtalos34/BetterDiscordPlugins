f82ca8d190764b35793c11017cdcd1cc3f289fc6
sirtalos34 <sirtalos34@gmail.com>
2020-11-05 14:17:37 +0100

    v1.0.7

--- BlurScreensharesOnMute.plugin.js
+++ BlurScreensharesOnMute.plugin.js
@@ -17,11 +17,15 @@ module.exports = (() =>
 				discord_id: '433027692372426753',
 				github_username: 'sirtalos34'
 			}],
-			version: '1.0.6',
+			version: '1.0.7',
 			description: 'Blurs all sceenshares and cameras when you mute yourself',
 			github: 'https://github.com/sirtalos34/BetterDiscordPlugins/blob/main/BlurScreensharesOnMute.plugin.js',
 			github_raw: 'https://raw.githubusercontent.com/sirtalos34/BetterDiscordPlugins/main/BlurScreensharesOnMute.plugin.js'
-		}
+		},
+		changelog: [{
+			'title': 'Added Keybind',
+			'items': [ 'You can now configure a keybind to toggle the blur' ]
+		}]
 	};
 
 	return !global.ZeresPluginLibrary ? class
@@ -60,7 +64,20 @@ module.exports = (() =>
 				{
 					super();
 					this.save = (m, e) => typeof this.settings[m] !== 'undefined' ? this.settings[m] = e : null;
-					this.defaultSettings = { blur: 35 };
+					this.defaultSettings = { blurKeybind: [], blurOnMute: true, blur: 35 };
+					this.globalShortcut = require('electron').remote.globalShortcut;
+					this._settings = {
+						generalSettings: {
+							name: 'General',
+							collapsible: false,
+							shown: true,
+							settings: {
+								blurKeybind: { type: 'Keybind', name: 'Keybind', exec: e => this.setupKeybind(e)  },
+								blurOnMute: { type: 'Switch', name: 'Activate on mute', exec: e => this.createMutationObserver() },
+								blur: { type: 'Slider', min: 1, max: 100, name: 'Blur Strength' }
+							}
+						}
+					}
 				}
 
 				onStart()
@@ -76,60 +93,111 @@ module.exports = (() =>
 
 				async onLogin()
 				{
-					const muteButton = document.querySelector('*[aria-label="Mute"]');
-					this.muteObserver = new MutationObserver(m =>
-					{
-						if(m[0].attributeName !== 'aria-checked') return;
-						const muted = (m[0].target.attributes['aria-checked'].nodeValue == 'true');
-						const css = `[class*="videoWrapper"], [class*="pictureInPictureWindow"] { filter: blur(${this.settings.blur}px) }`;
-						muted ? Api.PluginUtilities.addStyle(config.info.name, css) : Api.PluginUtilities.removeStyle(config.info.name);
-					});
-					this.muteObserver.observe(muteButton, { attributes: true });
+					this.setupKeybind(this.settings.blurKeybind);
+					if(this.settings.blurOnMute) this.createMutationObserver();
 				}
 
 				onStop()
 				{
-					if(this.muteObserver) this.muteObserver.disconnect();
+					try { this.globalShortcut.unregister(this.keycode); } catch(e) { Api.Logger.err('Failed to unregister global shortcut'); }
+					document.body.removeEventListener('keydown', this.listener, true);
+					if(this.MutationObserver) this.MutationObserver.disconnect();
 					Api.PluginUtilities.removeStyle(config.info.name);
 				}
 
 				getSettingsPanel()
 				{
 					const { Settings } = Api;
-					const set = {
-						generalSettings: {
-							name: 'General',
-							shown: true,
-							settings: {
-								blur: { type: 'Slider', min: 1, max: 100, name: 'Blur Strength', tooltip: null, exec: () => {} }
-							}
-						}
-					}
-
 					return Settings.SettingPanel.build(this.saveSettings.bind(this),
-						...Object.values(set).map(group => {
-							return new Settings.SettingGroup(group.name, { shown: group.shown || false }).append(
+						...Object.values(this._settings).map(group => {
+							const options = { collapsible: group.collapsible, shown: group.shown };
+							return new Settings.SettingGroup(group.name, options).append(
 								...Object.keys(group.settings).map(name => {
 									const i = group.settings[name];
-									let obj;
-									const exec = (e) => { this.save(name, e); i.exec(); };
+									const exec = (e) => { this.save(name, e); typeof i.exec === 'function' ? i.exec(e) : null; };
+									const args = [i.name, i.note, i.value || this.settings[name], exec, i.options || {}];
 									switch(i.type) {
-										case 'Switch':
-											obj = new Settings.Switch(i.name, null, this.settings[name], exec, i.options || {});
-											break;
+										case 'Dropdown': case 'RadioGroup':
+											args.splice(3, 0, i.values); break;
 										case 'Slider':
-											obj = new Settings.Slider(i.name, null, i.min, i.max, this.settings[name], exec, i.options || {});
-											break;
-										case 'Textbox':
-											obj = new Settings.Textbox(i.name, null, this.settings[name], exec, i.options || {});
-											break;
+											args.splice(2, 0, i.min); args.splice(3, 0, i.max); break;
+										case 'FilePicker':
+											args.splice(2, 1); break;
 									}
-									if(i.tooltip !== null) new Api.EmulatedTooltip(obj.inputWrapper, i.tooltip, { side: 'left' });
+									const obj = new Settings[i.type](...args);
+									if(typeof i.tooltip === 'string') new Api.EmulatedTooltip(obj.inputWrapper, i.tooltip, { side: 'left' });
 									return obj;
 								})
 							);
 						})
 					);
+				}
+
+				setupKeybind(keyCodes)
+				{
+					if(keyCodes.length === 0) return;
+					if(!keyCodes.includes(160) && !keyCodes.includes(162) && !keyCodes.includes(164))
+					{
+						this.settings.blurKeybind = [];
+						return Api.Toasts.error('Keybind must at least include one of the following: CTRL, SHIFT, ALT.');
+					}
+					if(keyCodes.filter(e => ![160, 162, 164].includes(e)).length === 0)
+					{
+						this.settings.blurKeybind = [];
+						return Api.Toasts.error('Keybind must include at least one key from A-Z, 0-9.');
+					}
+					if(keyCodes.length === 1)
+					{
+						this.settings.blurKeybind = [];
+						return Api.Toasts.error('Keybind must include at least two keys. Example: Ctrl+B');
+					}
+
+					if(this.keycode) try { this.globalShortcut.unregister(this.keycode); } catch(e) { Api.Logger.err('Failed to unregister global shortcut'); }
+					document.body.removeEventListener('keydown', this.listener, true);
+					const keys = [];
+					keyCodes.forEach(code => {
+						if(code === 162) return keys.push('CommandOrControl');
+						if(code === 160) return keys.push('Shift');
+						if(code === 164) return keys.push('Alt');
+						keys.push(String.fromCharCode(code));
+					});
+					this.keycode = keys.join('+');
+					let shortcut = false;
+					try { shortcut = this.globalShortcut.register(this.keycode, this.toggleBlur.bind(this)); } catch(e) { Api.Logger.err('Failed to register global shortcut'); }
+					if(shortcut) return;
+					this.listener = this.onKeyDown.bind(this);
+					document.body.addEventListener('keydown', this.listener, true);
+					Api.Logger.warn('Unable to register global shortcut. Registering local shortcut instead');
+				}
+
+				createMutationObserver()
+				{
+					const muteButton = document.querySelector('[aria-label="Mute"]');
+					this.MutationObserver = new MutationObserver(m =>
+					{
+						if(m[0].attributeName !== 'aria-checked') return;
+						const muted = (m[0].target.attributes['aria-checked'].nodeValue == 'true');
+						this.toggleBlur(muted);
+					});
+					this.MutationObserver.observe(muteButton, { attributes: true });
+				}
+
+				toggleBlur(state)
+				{
+					const injected = document.getElementById(config.info.name) !== null;
+					if(injected === state) return; if(typeof state !== 'boolean') state = !injected;
+					const css = `[class*="videoWrapper"], [class*="pictureInPictureWindow"] { filter: blur(${this.settings.blur}px) }`;
+					state ? Api.PluginUtilities.addStyle(config.info.name, css) : Api.PluginUtilities.removeStyle(config.info.name);
+				}
+
+				onKeyDown(e)
+				{
+					const codes = this.settings.blurKeybind;
+					if(codes.includes(162) && !e.ctrlKey) return;
+					if(codes.includes(160) && !e.shiftKey) return;
+					if(codes.includes(164) && !e.altKey) return;
+					if(codes[codes.length - 1] !== e.keyCode) return;
+					this.toggleBlur();
 				}
 			};
 		}
